45fea014ee13e7cdef2676eacfb4fa72da60befa
shamshodhamidov013-tech <shamshodhamidov013@gmail.com>
2026-02-04 13:15:37 +0500

    Update telegiram -bot.py

--- telegiram bot/telegiram -bot.py
+++ telegiram bot/telegiram -bot.py
@@ -2027,28 +2027,7 @@ async def send_video(message: types.Message):
             ] + [[InlineKeyboardButton(text="✅ Tekshirish", callback_data="check_subs")]]
         )
         await message.answer("Botdan foydalanish uchun quyidagi kanallarga obuna bo‘ling:", reply_markup=markup)
-   @dp.message(F.text == "79")
-async def send_video(message: types.Message):
-    user_id = message.from_user.id
-    
-    if await check_subs(user_id):  # Faqat obuna bo‘lganlarga javob qaytaradi
-        file_id = "BAACAgQAAxkBAAIH0mmC9yg0oEa5B3EHiuVghqJhF8cSAAJLHQAC6GX4U5GsboZnRuEyOAQ"
-        await message.answer_video(file_id, caption="""
-🗂 Yuklash: 6510
-        ⌨️ KOD: #79
-       bot; @UrtakKino_bot
-        kanal; @Tarjima_kinolar_uzb_tilda_z""")
-                                                        
-    else:
-        await message.answer('telegram kanalga obuna boling')
-        markup = InlineKeyboardMarkup(
-            inline_keyboard=[
-                [InlineKeyboardButton(text=f"🔗 {channel}", url=f"https://t.me/{channel[1:]}")] for channel in CHANNELS
-            ] + [[InlineKeyboardButton(text="✅ Tekshirish", callback_data="check_subs")]]
-        )
-        await message.answer("Botdan foydalanish uchun quyidagi kanallarga obuna bo‘ling:", reply_markup=markup)
-   
-
+ 
 async def main():
     print('bot ishladi....')
     await dp.start_polling(bot)
@@ -2056,5 +2035,6 @@ async def main():
 if __name__ == "__main__":
 
     asyncio.run(main())
+
 
 
